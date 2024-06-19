24232bc84a58719c0cc3171e77537d659c20e872
EugeniaDzadu <jeeniaace@gmail.com>
2024-06-19 11:03:45 +0000

    Second commit

--- src/components/FoodItem.vue
+++ src/components/FoodItem.vue
@@ -1,65 +1,48 @@
-<template>
+<!-- Components, Props -->
+<!-- <template>
+
+
   <div>
     <h2>
       {{ foodName }}
-      <img src="/img_quality.svg" v-show="isFavorite">
+      <img src="/img_quality.svg" v-show="foodIsFavorite">
     </h2>
     <p>{{ foodDesc }}</p>
+    <button v-on:click="toggleFavorite">Favorite</button>
   </div>
-  </template>
-  
-  <script>
-//   export default {
-//     data() {
-//       return {
-//         name: 'Apples',
-//         message: 'I like apples',
-//         clicks: 0
-//       }
-//     },
-//     methods: {
-//     countClicks() {
-//       this.clicks++;
-//     }
-//     }
-//   };
+</template>
 
-export default {
-    // props: [
-    //   'foodName', 'foodDesc', 'isFavorite'
-    // ]
-
-    props: {
-      foodName:{ 
-        type: String,
-        required: true
-      },
-      foodDesc:{ 
-        type: String,
-        required:false,
-        default: 'This is the default description.',
-        validator: function(value) {
-          if( 20<value.length && value.length<50 ) {
-            return true;
-          }
-          else {
-            return false;
-          }
-        }
-      },
-      isFavorite:{ 
-        type: Boolean,
-        required: false,
-        default:false
-      }
+<script>
+  export default {
+  props: ['foodName','foodDesc','isFavorite'],
+  data() {
+    return {
+      foodIsFavorite: this.isFavorite
     }
-
+  },
+  methods: {
+    toggleFavorite() {
+      this.foodIsFavorite = !this.foodIsFavorite;
+    }
+  }
   }
   </script>
   
   <style>
-      img {
-          height: 1.5em;
-          float: right;
-        }
-  </style>
+  img {
+    height: 1.5em;
+    float: right;
+  }
+</style>    -->
+
+<template>
+  <div>
+    <h2>{{ foodName }}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['foodName']
+  }
+</script>
